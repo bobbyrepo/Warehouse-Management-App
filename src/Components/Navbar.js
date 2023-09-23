3a04fd3d3c7ce6685eb6b9e5419062c22f2d233c
SC00bidu <you@example.com>
2023-09-23 21:59:20 +0530

    sticky navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,15 +4,15 @@ import logo from "../assets/logo.png";
 
 function Navbar() {
   return (
-    <nav>
-      <div className="sticky top-0 z-10 flex items-center bg-gray-100 h-[70px]">
+    <nav className="sticky top-0 z-10">
+      <nav className=" flex items-center bg-gray-100 h-[70px]">
         <div className="w-[80%] mx-auto">
           {/*------ Company Logo --------*/}
           <Link to="/">
             <img className="w-[160px]" src={logo} alt="" />
           </Link>
         </div>
-      </div>
+      </nav>
     </nav>
   );
 }
